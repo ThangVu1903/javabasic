265cb13724317d63191d3ddbeb363f1857b4c207
ThangVu1903 <vuthang19032002az@gmail.com>
2021-08-10 16:57:53 +0700

    first commit

--- src/com/javabasic/stringlesson/lesson2/LargestNumber.java
+++ src/com/javabasic/stringlesson/lesson2/LargestNumber.java
@@ -4,33 +4,29 @@ import java.util.Scanner;
 
 public class LargestNumber {
     public static void main(String[] args) {
-        int numberFirst, numberLast;
-        numberFirst= enterFirstNumber();
-        numberLast = enterLastNumber();
+        int numberFirst= enterNumber();
+        int numberLast = enterNumber();
         int great = maxNumber(numberFirst,numberLast);
         System.out.println("the largest number is: "+great);
 
 
     }
-    public static int enterFirstNumber(){
+
+
+    public static int enterNumber(){
         Scanner scanner=new Scanner(System.in);
-        System.out.println("enter number one :");
-        int numberOne = scanner.nextInt();
-        return numberOne;
+        System.out.println("enter number :");
+        int number = scanner.nextInt();
+        return number;
     }
-    public static int enterLastNumber(){
-        Scanner scanner=new Scanner(System.in);
-        System.out.println("enter number two:");
-        int numberTwo = scanner.nextInt();
-        return numberTwo;
-    }
+
+
     public static int maxNumber(int numberOne, int numberTwo){
         int bigger;
         if (numberOne>numberTwo){
              bigger = numberOne;
         }
         else {
-
             bigger = numberTwo;
         };
 
